05b5e7d64420a1836fb2cf429a10669758e3cb4e
OmPals <ompalsanawala241099@gmail.com>
2024-12-21 18:17:36 +0530

    remove: separate search and rescard list component

--- src/Homepage/Body.js
+++ src/Homepage/Body.js
@@ -1,14 +1,39 @@
 import { useState } from "react";
-import ResCardsList from "./ResCardList";
-import Search from "./Search";
+import { restaurants } from "../../dummy_data_specific.json";
+import { IMG_BASE_URL as imgBaseURL } from "../../constants/base_urls";
+import ResCard from "./ResCard";
 
 const Body = () => {
-  const [isFilterOn, setIsFilterOn] = useState(false);
+  const tempResArray = restaurants.map((res) => {
+    return {
+      title: res.info?.name || "Restaurant name",
+      addr: res.info?.locality || "Some dummy address",
+      dist: res.info?.sla.slaString,
+      img: `${imgBaseURL}${res.info.cloudinaryImageId}`,
+      ratings: Math.round(res.info?.avgRating),
+      id: res.info.id,
+    };
+  });
+  const [restaurantsList, setRestaurantsList] = useState(tempResArray);
+
+  const filterBtnOnClick = () =>
+    setRestaurantsList(tempResArray.filter((res) => res.ratings > 4));
 
   return (
     <div className="body">
-      <Search setIsFilterOn={setIsFilterOn} isFilterOn={isFilterOn} />
-      <ResCardsList isFilterOn={isFilterOn} />
+      <div className="search-container">
+        <button className="filter-btn" onClick={filterBtnOnClick}>
+          🏆 filter top restaurants 🏆
+        </button>
+        <div>
+          <span className="search-icon">🔍</span>
+        </div>
+      </div>
+      <div className="res-cards-list">
+        {restaurantsList.map((res) => {
+          return <ResCard key={res.id} resData={res} />;
+        })}
+      </div>
     </div>
   );
 };
